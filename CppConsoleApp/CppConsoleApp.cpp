a33b98ba3fe52ed6f6dee3d58de26b2e72c2bd31
avani <avanidhar@gmail.com>
2015-03-20 20:54:46 -0700

    C++ projects

---
+++ CppConsoleApp/CppConsoleApp.cpp
@@ -0,0 +1,12 @@
+// CppConsoleApp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+using namespace std;
+int main()
+{
+	cout << "Hello World"<<endl;
+	getchar();
+	return 0;
+}
+
